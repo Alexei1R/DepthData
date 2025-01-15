6c67497ecb86c3b7f50fd371e4e2c9ed8c5226d2
cruasanya <98823717+cruasanya@users.noreply.github.com>
2025-01-15 18:56:14 +0200

    add viewcontroller for login page

--- ArpalusSample/ViewController.swift
+++ ArpalusSample/ViewController.swift
@@ -9,7 +9,46 @@ import UIKit
 import ARKit
 import RealityKit
 import SceneKit
+import SwiftUI
+import ARPalusSDK
 
-final class RootNavigation: UINavigationController {
-    
+//class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let loginPage = LoginPage(, onSignIn: <#() -> Void#>)
+//        let hostingController = UIHostingController(rootView: loginPage)
+//        addChild(hostingController)
+//        view.addSubview(hostingController.view)
+//
+//        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+//                NSLayoutConstraint.activate([
+//                    hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+//                    hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//                    hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//                    hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//        ])
+//
+//        // Finalize the child view controller addition
+//        hostingController.didMove(toParent: self)
+//    }
+//
+//    func proceed() {
+//        navigationController?.pushViewController(ScanningViewController(), animated: true)
+//    }
+//}
+//
+class ViewController: UIHostingController<LoginPage> {
+    init() {
+        super.init(rootView: LoginPage(onSignIn: {}))
+        rootView = LoginPage(onSignIn: proceed)
+    }
+
+    @MainActor @preconcurrency required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func proceed() {
+        navigationController?.pushViewController(ScanningViewController(), animated: true)
+    }
 }
