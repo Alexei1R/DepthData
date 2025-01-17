9fcf036ae1ff039a7d98c08cd8d8ff4c359af5a5
cruasanya <98823717+cruasanya@users.noreply.github.com>
2025-01-17 12:24:53 +0200

    add image service and local storage to sdk environment

--- ARPalusSDK/SDKEnvironment.swift
+++ ARPalusSDK/SDKEnvironment.swift
@@ -2,13 +2,11 @@ import Foundation
 import FirebaseFirestore
 
 public enum ArpalusSDK {
-    public static func start(email: String, password: String ) {
+    public static func start(email: String, password: String , completion: @escaping () -> Void) {
         Task { @MainActor in
             do {
                 let result = try await SDKEnvironment.shared.authentication.authenticate(email: email, password: password)
                 print(result)
-//                let doc = try await Firestore.sdk.collection("Users").document(email).getDocument(as: UserSettings.self)
-//                print(doc)
                 let userSettings = try await SDKEnvironment.shared.settingsStore.getUserSettings()
                 print("User settings: ", userSettings)
                 let appSettings = try await SDKEnvironment.shared.settingsStore.getAppSettingsVersion()
@@ -17,23 +15,26 @@ public enum ArpalusSDK {
                 try await SDKEnvironment.shared.settingsStore.getDeployments()
                 try await SDKEnvironment.shared.settingsStore.downloadProjects()
             } catch {
-                print("Error in starting sdk", error.localizedDescription)
+                print("Error in starting sdk: ", error.localizedDescription)
             }
+            completion()
         }
     }
-//    public static func getViewController() -> ScanningViewController {
-//
-//    }
 }
 
 struct SDKEnvironment {
+    var localStorage: LocalStorage
     var authentication: Authentication
     var settingsStore: SettingsStore
+    var imageSevice: ImageService
 
     static var shared: SDKEnvironment = {
-        SDKEnvironment(
+        let localStorage: LocalStorage = LocalStorage()
+        return SDKEnvironment(
+            localStorage: localStorage,
             authentication: Authentication(),
-            settingsStore: SettingsStore()
+            settingsStore: SettingsStore(localStorage: localStorage),
+            imageSevice: ImageService(localStorage: localStorage)
         )
     }()
 }
