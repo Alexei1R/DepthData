136d62605e1759ca1e29f480e36d89129f8262fb
cruasanya <98823717+cruasanya@users.noreply.github.com>
2025-01-21 10:46:22 +0200

    update login page ui

--- ArpalusSample/LoginPage.swift
+++ ArpalusSample/LoginPage.swift
@@ -37,66 +37,128 @@ struct LoginPage: View {
         case signUp
     }
 
-    @State var email: String = "victor@thenoughtyfox.com"
-    @State var password: String = "123456"
+    @State var email: String = ""
+    @State var password: String = ""
     @State var mode: Mode = .signIn
+    @State private var isSecure: Bool = true
 
     var onSignIn: (String, String) -> Void
 
     var body: some View {
         VStack() {
             Spacer()
-            VStack(alignment: .leading) {
+            Image(.arpalusFullLogo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Email")
+                    .padding(.top, 50)
                 TextField("Email", text: $email)
-                Divider()
-                TextField("Password", text: $password)
-                Divider()
+                    .keyboardType(.emailAddress)
+                    .frame(height: 50)
+                    .padding(.leading)
+                    .background(
+                        Capsule()
+                            .stroke(style: StrokeStyle(lineWidth: 2))
+                            .foregroundStyle(Color(.lightGray))
+                    )
+                Text("Password")
+                ZStack {
+                    if isSecure {
+                        SecureField("******", text: $password)
+                            .frame(height: 50)
+                            .padding(.leading)
+                            .background(
+                                Capsule()
+                                    .stroke(style: StrokeStyle(lineWidth: 2))
+                                    .foregroundStyle(Color(.lightGray))
+                            )
+                    } else {
+                        TextField("******", text: $password)
+                            .frame(height: 50)
+                            .padding(.leading)
+                            .background(
+                                Capsule()
+                                    .stroke(style: StrokeStyle(lineWidth: 2))
+                                    .foregroundStyle(Color(.lightGray))
+                            )
+                    }
+                    HStack {
+                        Spacer()
+                        Image(systemName: isSecure ? "eye" : "eye.slash")
+                            .foregroundStyle(Color(.lightGray))
+                            .onTapGesture {
+                                isSecure.toggle()
+                            }
+                            .padding(.trailing)
+                    }
+                }
+                .animation(.easeInOut, value: isSecure)
+                HStack {
+                    Spacer()
+                    Button {
+                        // Firebase reset password request
+                    }
+                    label: {
+                        Text("Forgot Password ?")
+                            .foregroundStyle(.blue)
+                    }
+
+                }
+                Button(action: {
+                    switch mode {
+                    case .signIn:
+                        UserStore().signIn(email: email, password: password) { result in
+                            switch result {
+                            case .success:
+                                print("Sign in successful")
+                                onSignIn(email, password)
+                            case .failure(let error):
+                                print("Sign in failed: \(error)")
+                            }
+                        }
+                    case .signUp:
+                        UserStore().signUp(email: email, password: password) { result in
+                            switch result {
+                            case .success:
+                                print("Sign up successful")
+                                onSignIn(email, password)
+                            case .failure(let error):
+                                print("Sign up failed: \(error)")
+                            }
+                        }
+                    }
+                }) {
+                    HStack {
+                        if mode == .signIn {
+                            Text("Sign In")
+                        } else {
+                            Text("Sign Up")
+                        }
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .background(
+                        Capsule()
+                            .foregroundStyle(.blue)
+                    )
+                }
+                Spacer()
             }
             .foregroundStyle(.black)
-            Button(action: {
-                switch mode {
-                case .signIn:
-                    UserStore().signIn(email: email, password: password) { result in
-                        switch result {
-                        case .success:
-                            print("Sign in successful")
-                            onSignIn(email, password)
-                        case .failure(let error):
-                            print("Sign in failed: \(error)")
-                        }
-                    }
-                case .signUp:
-                    UserStore().signUp(email: email, password: password) { result in
-                        switch result {
-                        case .success:
-                            print("Sign up successful")
-                            onSignIn(email, password)
-                        case .failure(let error):
-                            print("Sign up failed: \(error)")
-                        }
-                    }
-                }
-            }) {
-                HStack {
-                    if mode == .signIn {
-                        Text("Sign In")
-                    } else {
-                        Text("Sign Up")
-                    }
-                }
-                .padding(.vertical)
-                .frame(maxWidth: .infinity)
-                .foregroundStyle(.white)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.blue)
-                )
-            }
-            Spacer()
+            .padding(.horizontal, 24)
+            .background(
+                Color.white
+                    .clipShape(UnevenRoundedRectangle(cornerRadii: .init(topLeading: 20, topTrailing: 20)))
+                    .ignoresSafeArea()
+            )
+
         }
-        .padding(.horizontal, 24)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
+        .background(Color.blue.opacity(0.2))
     }
 }
 
